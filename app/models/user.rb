ff62d4da1bd7afd2835a9d9bbd8d7dab75bcb744
Steve Braun <sabraun@madisoncollege.edu>
2013-03-07 15:05:23 -0600

    lab8.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@
 #  email      :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#
+#   
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
@@ -21,6 +21,6 @@ class User < ActiveRecord::Base
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 end
