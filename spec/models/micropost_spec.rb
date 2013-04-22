6ae39f3a986ff535b0114748e03ecedf3423c785
Steve Braun <sabraun@madisoncollege.edu>
2013-04-21 21:59:32 -0500

    L10.24 pass97

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -12,9 +12,9 @@ describe Micropost do
   it { should respond_to(:user) }
   its(:user) { should == user }
   
-
-
-
+   
+   
+   
   it { should be_valid }
 
   describe "accessible attributes" do
@@ -24,10 +24,36 @@ describe Micropost do
       end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
     end    
   end
-
+   
+     
   describe "when user_id is not present" do
     before { @micropost.user_id = nil }
     it { should_not be_valid }
   end
+  
+  
+  
+  describe "when user_id is not present" do
+    before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "with blank content" do
+    before { @micropost.content = " " }
+    it { should_not be_valid }
+  end
+
+  describe "with content that is too long" do
+    before { @micropost.content = "a" * 141 }
+    it { should_not be_valid }
+  end
+
+  
+  
+  
+  
+  
+  
+  
 end
 
